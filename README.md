ef7e8343fa844b8888171bec6709c4a86ff465da
Stian Indal Haugseth <git.modbus-registry-scanner@stians.com>
2019-12-30 20:50:23 +0100

    Added CSV-creation for Excel-readable files.

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # modbus-registry-scanner
-Quick and dirty script in Java to scan multiple Modbus registries in bulk
 
-I needed this so it's quick and dirty so please exuse the structure. Please use as you see fit for your own needs.
+I needed this so it's quick and dirty so please excuse the structure. Please use as you see fit for your own needs.
+
+It was developed to query all known registers as found in Systemairs Variable list (PDF) for their SAVE ventilators. I have included a CSV-version of all registers found in the file.
 
 This is a complete IntelliJ-project. If you are not familiar with Java, and/or IntelliJ I suggest you start with downloading the IntelliJ Community IDE and opening the project folder. Developed and tested with Java 8.
+
+Code files are:
+* src/main/java/ScanRegistersFromCsv.java
+* src/main/java/ScanRegisterByRange.java
+
 
 Usage:
 1. Modify src/main/java/ScanRegistersFromCsv.java and/or src/main/java/ScanRegisterByRange.java with ip address and port
--- src/main/java/ScanRegistersFromCsv.java
+++ src/main/java/ScanRegistersFromCsv.java
@@ -6,9 +6,7 @@ import de.re.easymodbus.exceptions.FunctionCodeNotSupportedException;
 import de.re.easymodbus.exceptions.ModbusException;
 import de.re.easymodbus.modbusclient.ModbusClient;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 public class ScanRegistersFromCsv {
@@ -16,7 +14,9 @@ public class ScanRegistersFromCsv {
 
 
         try {
-            FileReader fileReader = new FileReader("D:\\home\\strips\\Documents\\systemair.csv");
+            FileReader fileReader = new FileReader("resources/systemair_SAVE_Modbus_Variable_List_20190116_r29.csv");
+            FileWriter csvVariablesWithValues = new FileWriter("resources/modbus_variables_values.csv");
+
 
             CSVReader reader = new CSVReader(fileReader);
             List<String[]> csvFileList = reader.readAll();
@@ -26,40 +26,60 @@ public class ScanRegistersFromCsv {
             ModbusClient modbusClient = new ModbusClient("10.10.100.18", 502);
             modbusClient.Connect();
 
+            String registerCode;
+            String signed;
+            String registerType;
+            String registerFlag;
+            Integer address;
+            String min;
+            String max;
+            String description;
+
+            Integer value = null;
+            String status = "";
+
             for (int i = 0; i < csvFileList.size(); i++) {
                 String[] csvLineArray = csvFileList.get(i);
                 if (i > 0) {
-                    String registerCode = csvLineArray[0];
-                    String signed = csvLineArray[1];
-                    String registerType = csvLineArray[2];
-                    String registerFlag = csvLineArray[3];
-                    Integer address = Integer.parseInt(csvLineArray[4]);
-                    String min = csvLineArray[5].replaceAll("\n", "");
-                    String max = csvLineArray[6].replaceAll("\n", "");
-                    String description = csvLineArray[7];
 
-                    Integer value = null;
-                    String status = "";
+                    registerCode = csvLineArray[0];
+                    signed = csvLineArray[1];
+                    registerType = csvLineArray[2];
+                    registerFlag = csvLineArray[3];
+                    address = Integer.parseInt(csvLineArray[4]);
+                    min = csvLineArray[5].replaceAll("\n", "");
+                    max = csvLineArray[6].replaceAll("\n", "");
+                    description = csvLineArray[7];
+
+                    value = null;
+                    status = "";
 
                     if (registerType.contains("Read Input Register")) {
                         try {
                             value = modbusClient.ReadInputRegisters(address, 1)[0];
                             status = "OK";
                         } catch (FunctionCodeNotSupportedException e) {
-                            status = "ReadInputRegisters returned FunctionCodeNotSupportedException, try ReadHoldingRegisters";
+                            // Workaround, either the server does not match the variable list or it seems InputRegisters can be read as HoldingRegisters
+                            status = "ReadInputRegisters returned FunctionCodeNotSupportedException, trying ReadHoldingRegisters";
                         }
                     }
 
+                    // Get HoldingRegister if HoldingRegister or status is not empty, if status is not empty InputRegister query failed
                     if ((registerType.contains("Holding Register")) || (!status.isEmpty())) {
                         value = modbusClient.ReadHoldingRegisters(address, 1)[0];
                     }
 
                     System.out.println(("\"" + registerCode + "\",\"" + signed + "\",\"" + registerType + "\",\"" + registerFlag + "\",\"" + address + "\",\"" + min + "\",\"" + max + "\",\"" + description + "\",\"" + value + "\",\"" + status + "\"").replaceAll("\"\"",""));
+                    csvVariablesWithValues.append(("\"" + registerCode + "\",\"" + signed + "\",\"" + registerType + "\",\"" + registerFlag + "\",\"" + address + "\",\"" + min + "\",\"" + max + "\",\"" + description + "\",\"" + value + "\",\"" + status + "\"").replaceAll("\"\"","") + "\n");
+
                 } else {
                     System.out.println(java.util.Arrays.toString(csvLineArray).replaceAll("\\[|\\]|\\s","") + ",value,status");
+                    csvVariablesWithValues.write(java.util.Arrays.toString(csvLineArray).replaceAll("\\[|\\]|\\s","") + ",value,status\n");
                 }
+                csvVariablesWithValues.flush();
             }
 
+            csvVariablesWithValues.close();
             modbusClient.Disconnect();
 
 
